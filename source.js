5c8dde2b8de742e1ab993266ba594e0533115e34
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-04-22 22:32:32 -0700

    xmlhttpRequest framework an logging ready

--- source.js
+++ source.js
@@ -4,8 +4,38 @@ function searchClick() {
     console.log("button pressed");
 }
 
-function color(){
+function color() {
     var below = document.getElementById("below");
     below.style.backgroundColor = "blue";
     console.log('onload ran');
 }
+
+function getGists() {
+    
+    console.log("getGists running");
+    
+    var url = 'https://api.github.com/gists';
+    var gistReq = new XMLHttpRequest();
+    
+    if (!gistReq) {
+        throw 'unable to create XMLHttpRequest';
+    }
+    
+    gistReq.onreadystatechange = function () {
+        console.log(this.readyState, this.status, this.statusText);
+        if (this.readyState === 4 && this.status === 200) {
+            console.log("request done");
+            
+            if (this.resoponseText) {
+                var GistsFeed = JSON.parse(this.resoponseText);
+            } else {
+                throw 'no response text';
+            }
+            console.log(this.resoponseText);
+            console.log(GistsFeed.toString());
+        }
+    };
+    gistReq.open('GET', url);
+    gistReq.send();
+}
+    
