676d1a8dafef95b758b270911adb4f102006116e
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-04-23 15:20:36 -0700

    asserting that all gists have at least one file, and testing acesss of avatar_url

--- source.js
+++ source.js
@@ -1,44 +1,55 @@
-function searchClick() {
-    var below = document.getElementById("below");
-    below.textContent = 'button pressed';
-    console.log("button pressed");
+function loadFavorites() {
+    //TODO implement
+    console.log('onload called loadFavorites');
 }
 
-function color() {
-    var below = document.getElementById("below");
-    below.style.backgroundColor = "blue";
-    console.log('onload ran');
-}
 
+//attached to search button
 function getGists() {
-    
-    console.log("getGists running");
-    
+
+    console.log('getGists running');
+
     var url = 'https://api.github.com/gists';
     var gistReq = new XMLHttpRequest();
-    
+    var GistsFeed;
+
     if (!gistReq) {
         throw 'unable to create XMLHttpRequest';
     }
-    
+
     gistReq.onreadystatechange = function () {
         console.log(this.readyState, this.status, this.statusText);
         if (this.readyState === 4 && this.status === 200) {
-            console.log("request done");
-            
+            console.log('request done');
+
             if (this.response) {
-                var GistsFeed = JSON.parse(this.response);
+                GistsFeed = JSON.parse(this.response);
             } else {
                 throw 'no response';
             }
             //console.log(this.response);
             //console.log(GistsFeed.toString());
-            GistsFeed.forEach(function(g){
-                console.log(g.url);
+            GistsFeed.forEach(function (g) {
+                //console.log(g.description);
+                if (g.hasOwnProperty('owner')) {
+                    console.log(g.owner.avatar_url);
+                }
+                
+                if (!g.hasOwnProperty('files')) {
+                    console.log('this one has no file');
+                } else {
+                    
+                    //g.files.forEach(function (f) {
+                    //    if (!f.hasOwnProperty('language')) {
+                    //        console.log('this file has no language');
+                    //    }
+                    //});
+                }
+                
             });
         }
     };
     gistReq.open('GET', url);
     gistReq.send();
 }
-    
+
