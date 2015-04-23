27303a553559a68c8e64018309b7a5ef81e419da
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-04-22 21:59:16 -0700

    making sure html and js file were linked,  forgot () at end of function name at first, working now

---
+++ source.js
@@ -0,0 +1,11 @@
+function searchClick() {
+    var below = document.getElementById("below");
+    below.textContent = 'button pressed';
+    console.log("button pressed");
+}
+
+function color(){
+    var below = document.getElementById("below");
+    below.style.backgroundColor = "blue";
+    console.log('onload ran');
+}
